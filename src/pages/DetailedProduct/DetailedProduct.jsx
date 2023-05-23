6d387f46202afa53f56417beedc0c819f05d3038
Vazgen <d.2100.v@gmail.com>
2023-05-23 23:57:30 +0400

    add changes

--- src/pages/DetailedProduct/DetailedProduct.jsx
+++ src/pages/DetailedProduct/DetailedProduct.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "./index.css";
 
 const DetailedProduct = () => {
+  const [porduct, setProduct] = useState({});
   const { moyId } = useParams();
-  return <div>DetailedProduct {moyId}</div>;
+
+  const getProducts = async () => {
+    const data = await fetch(`https://dummyjson.com/products/${moyId}`);
+    await data.json().then((data) => {
+      setProduct(() => {
+        return {
+          ...data,
+        };
+      });
+    });
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  console.log({ porduct });
+
+  return <div className="DetailedProduct"></div>;
 };
 
 export default DetailedProduct;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -22,18 +22,18 @@ function Home() {
   return (
     <div className="wrapper">
       {products.map((product, idx) => {
-        const { title, images, price } = product;
+        const { title, images, price, id } = product;
 
         const realText =
           title.length > 10 ? `${title.slice(0, 10)} ...` : title;
 
         return (
-          <div className="product" key={idx}>
+          <a href={`/product/${id}`} className="product" key={idx}>
             <img className="product__img" src={images[0]} alt={title} />
             <p className="product__title">{realText}</p>
             <p className="product__price">{price}</p>
             <button className="btn">Buy Now</button>
-          </div>
+          </a>
         );
       })}
     </div>
